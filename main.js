66d1ae7ba7db18f6004de7a2aefde4a1f16fe615
Lucas Santos <lucassnts963@gmail.com>
2023-05-05 08:43:37 -0300

    adiciona funcionalidade de ouvir as alteraçãos do clipboard

--- main.js
+++ main.js
@@ -1,21 +1,35 @@
-const { resolve, basename } = require('path')
+const { resolve } = require('path')
 const { app, Tray, Menu, clipboard } = require('electron')
 
 let mainTray = {}
 
-let clipboardHistory = [
-    { label: 'Lucas', type: 'normal', click: ({ label }) => clipboard.writeText(label) },
-]
+let clipboardHistory = []
+
+let lastClipboardText = ''
 
 app.whenReady().then(() => {
     mainTray = new Tray(resolve(__dirname, 'assets', 'iconTemplate.png'))
     
     render(mainTray)
+
+    clipboardWatcher()
 })
 
 function render (tray = mainTray) {
-
     const contextMenu = Menu.buildFromTemplate(clipboardHistory)
     tray.setToolTip('This is my application.')
     tray.setContextMenu(contextMenu)
+}
+
+function clipboardWatcher() {
+    setInterval(() => {
+        let currentClipboardText = clipboard.readText()
+
+        if (lastClipboardText !== currentClipboardText) {
+            clipboardHistory.push({ label: currentClipboardText, type: 'normal', click: ({ label }) => clipboard.writeText(label) })
+            render()
+        }
+
+        lastClipboardText = currentClipboardText
+    }, 1000)
 }
